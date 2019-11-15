3cb64ff2763ecda31eac3737db88aeae2563bf39
Sebastien Roche <sebastien.roche@mail.utoronto.ca>
2019-11-15 10:49:39 -0500

    changed anaconda2 to anaconda3

--- install_tccon_netcdf.sh
+++ install_tccon_netcdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $(which conda | wc -l) == 0 ] ; then
-        source $HOME/anaconda2/etc/profile.d/conda.sh
+        source $HOME/anaconda3/etc/profile.d/conda.sh
 else
         conda_base=$(conda info --base)
         source $conda_base/etc/profile.d/conda.sh
