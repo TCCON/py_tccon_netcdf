db3f21ba20720f9d2978b7b8818d7b8427d07e85
Joshua Laughner <jlaugh@caltech.edu>
2020-04-13 14:46:59 -0700

    Modified install_tccon_netcdf.sh to use bash login to activate conda and to ignore ~/.pydistutils.cfg files

--- install_tccon_netcdf.sh
+++ install_tccon_netcdf.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
-if [ $(which conda | wc -l) == 0 ] ; then
-        source $HOME/anaconda3/etc/profile.d/conda.sh
+#!/bin/bash -l
+
+if [ $(basename $CONDA_PREFIX) != 'ggg-tccon-default' ]; then
+    echo "In $0: must activate ggg-tccon-default environment"
+    # should be available because we run this in a login shell
+    conda activate ggg-tccon-default
 else
-        conda_base=$(conda info --base)
-        source $conda_base/etc/profile.d/conda.sh
+    echo "In $0: ggg-tccon-default environment already active"
 fi
 
 if [ ! -d scripts ]; then
     mkdir -v scripts
 fi
 
-python setup.py develop --script-dir=./scripts && mv -v ./scripts/write_netcdf $GGGPATH/bin/
+# develop: do not copy to $SITEDIR/, just run from here
+# --no-user-cfg: ignore any ~/.pydistutils.cfg file
+# --script-dir: write command line scripts to the given directory
+python setup.py develop --no-user-cfg --script-dir=./scripts && mv -v ./scripts/write_netcdf $GGGPATH/bin/
