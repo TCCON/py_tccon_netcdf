f81af6376e434c8567b1650ea6ccd5d3edfbb8b6
Sebastien Roche <sebastien.roche@mail.utoronto.ca>
2020-09-25 12:49:24 -0400

    added MIT license

--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as infile:
+    long_description = infile.read()
+
 setup(
     name='write_tccon_netcdf',
     description='Write official TCCON netCDF output file',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Sebastien Roche',
     author_email='sebastien.roche@mail.utoronto.ca',
     version='1.0.0',
-    url='',
+    url='https://bitbucket.org/rocheseb/tccon_netcdf',
     install_requires=[
         'netcdf4>=1.5.0',
         'numpy>=1.16.0',
         'pandas>=0.23.0'
         ],
     packages=find_packages(),
+    classifiers=[
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
     entry_points={'console_scripts': [
         'write_netcdf=write_tccon_netcdf.write_netcdf:main',
         'compare_netcdf=write_tccon_netcdf.write_netcdf:compare_nc_files_command_line'
-    ]}
+    ]},
+    license='MIT',
+    python_requires='>=3.7',
 )
