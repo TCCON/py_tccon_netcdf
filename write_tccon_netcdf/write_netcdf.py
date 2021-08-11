99009d29b2f46a2d76dc19e6d99ea2553ffc6d45
Joshua Laughner <jlaugh@caltech.edu>
2021-08-11 11:31:46 -0700

    Added warning if GGGPATH does not match runlog
    This should make it clearer when `write_netcdf` fails because
    it can't find the .lse file.

--- write_tccon_netcdf/write_netcdf.py
+++ write_tccon_netcdf/write_netcdf.py
@@ -1091,6 +1091,7 @@ def get_runlog_file(GGGPATH,tav_file,col_file):
     if runlog_file.startswith(GGGPATH):
         lse_file = os.path.join(GGGPATH,'lse','gnd',os.path.basename(runlog_file).replace('.grl','.lse'))
     else:
+        logging.warning('Path to runlog ({}) does not start with GGGPATH ({}), may not be able to find the .lse file. If so, make sure your GGGPATH environmental variable is set correctly.'.format(runlog_file, GGGPATH))
         lse_file = runlog_file.replace('.grl','.lse')
     
     if not os.path.exists(lse_file):
