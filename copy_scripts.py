62d62728188be9728e570aca983564075ec7b7d3
Joshua Laughner <jlaugh@caltech.edu>
2026-01-30 12:55:29 -0800

    Two changes to support GGG2020.1 installation:
    1. `copy_scripts.py` now uses `toml` (included in the GGG environment
       file) instead of `tomli`.
    2. License declaration in `pyproject.toml` follows current Python
       conventions to avoid loud warnings during installation.

--- copy_scripts.py
+++ copy_scripts.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from pathlib import Path
 import shutil
 import sys
-import tomli
+import toml
 
 
 def main():
@@ -12,15 +12,15 @@ def main():
     p.add_argument('--pyproject-file', default='./pyproject.toml',
                    help='pyproject.toml file to get the script names from. Default = %(default)s')
     p.add_argument('--env-prefix', help='Prefix for the environment the scripts were installed in, '
-                                      'this would be the directory containing the bin directory with the scripts. '
-                                      'If not given, the currently active environment is used')
-    
+                                        'this would be the directory containing the bin directory with the scripts. '
+                                        'If not given, the currently active environment is used')
+
     clargs = vars(p.parse_args())
     driver(**clargs)
 
 def driver(dest, pyproject_file='./pyproject.toml', env_prefix=None):
-    with open(pyproject_file, 'rb') as f:
-        cfg = tomli.load(f)
+    with open(pyproject_file, 'r') as f:
+        cfg = toml.load(f)
 
     env_prefix = env_prefix or sys.prefix
     bin_dir = Path(env_prefix) / 'bin'
